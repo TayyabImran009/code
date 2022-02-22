1e9c6a5ea65925e67aaf104178a3366300ed124f
Tayyab <F2017065009@umt.edu.pk>
2022-02-22 23:59:05 +0500

    new db

--- project1/settings.py
+++ project1/settings.py
@@ -102,13 +102,32 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'dbmaster',
+
+        'USER': 'dbmasteruser',
+
+        'PASSWORD': "M<c5|ku[S>sRTQ;<7?BE[,u1af>FN7H&",
+
+        'HOST': 'ls-bfa172f45a7b25a5f641c6a967d396c4f2a77a5d.cid41ls7iqzm.eu-west-3.rds.amazonaws.com',
+
+        'PORT': '3306',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- validaciones/admin.py
+++ validaciones/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import *
 
+class ValidationAdmin(admin.ModelAdmin):
+    list_display = ('reference', 'make', 'model','amount_purchase','amount_sale','margin','type','risk')
+
+class VatAdmin(admin.ModelAdmin):
+    list_display = ('description', 'amount')
+
+class CostAdmin(admin.ModelAdmin):
+    list_display = ('description', 'amount','active')
+
+class CostQuantityAdmin(admin.ModelAdmin):
+    list_display = ('quantity','amount')
+
 # Register your models here.
-admin.site.register (Vat)
-admin.site.register (Cost)
-admin.site.register (CostQuantity)
-admin.site.register (Validation)
+admin.site.register (Vat,VatAdmin)
+admin.site.register (Cost,CostAdmin)
+admin.site.register (CostQuantity,CostQuantityAdmin)
+admin.site.register (Validation,ValidationAdmin)
 
